c5bf9189d4bd06bc9e1f2826c7283983b039c767
niliqiang <ni_liqiang@126.com>
2019-07-20 16:39:33 +0800

    使用 Mybatis Generator 插件生成实体类、Mapper接口以及对应的XML文件

---
+++ src/main/java/com/dataprocess_server/DataProcessServerApplication.java
@@ -0,0 +1,13 @@
+package com.dataprocess_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProcessServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataProcessServerApplication.class, args);
+    }
+
+}
